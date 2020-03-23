f28d8a67a2f8d097e5f4ff2cf0ce3720c494dc8b
johnne <john.sundh@scilifelab.se>
2020-03-23 12:19:15 +0100

    Clean up unused utils

--- source/utils/dag2png.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-bgcolor=$2
-
-if [ "$bgcolor" == "" ] ; then
-  bgcolor='transparent'
-fi
-
-cat $1 | sed "s/bgcolor=white/bgcolor=$bgcolor/g" | sed 's/rounded"/rounded", fontcolor=white/g' | sed 's/rounded,dashed"/rounded,dashed", fontcolor=white/g' | dot -Tpng -Gdpi=300
