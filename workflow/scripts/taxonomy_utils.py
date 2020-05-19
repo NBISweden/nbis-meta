b2fada116fefa67b1a8cb8e12c06a16f9129b89c
johnne <john.sundh@scilifelab.se>
2020-05-19 14:42:55 +0200

    Add tango assign function to utils

--- workflow/scripts/taxonomy_utils.py
+++ workflow/scripts/taxonomy_utils.py
@@ -81,8 +81,37 @@ def tango_mash(sm):
         fhout..write("Added:       {}\n".format(stats['added']))
 
 
+def tango_assign_orfs(sm):
+    """
+    Transfers taxonomic assignments from contigs down to ORFs called on contigs
+    :param sm: snakemake object
+    :return:
+    """
+    gff_df=pd.read_csv(sm.input.gff, header=None, sep="\t", comment="#",
+                           usecols=[0, 8], names=["contig", "id"])
+    # Extract ids
+    ids=["{}_{}".format(gff_df.loc[i, "contig"],
+                        gff_df.loc[i, "id"].split(";")[0].split("_")[-1]) for i in gff_df.index]
+    gff_df.loc[:, "id"]=ids
+    # Read taxonomy for contigs
+    tax_df=pd.read_csv(sm.input.tax, header=0, sep="\t", index_col=0)
+    # Merge dataframes
+    orf_tax_df=pd.merge(gff_df, tax_df, left_on="contig",
+                        right_index=True, how="outer")
+    # When using 'outer' merging there may be contigs with no called ORF
+    # but with a tax assignment. Drop these contigs.
+    orf_tax_df=orf_tax_df.loc[orf_tax_df["id"]==orf_tax_df["id"]]
+    # Set Unclassified for NA values
+    orf_tax_df.fillna("Unclassified", inplace=True)
+    # Set index to ORF ids
+    orf_tax_df.set_index("id", inplace=True)
+    orf_tax_df.drop("contig", axis=1, inplace=True)
+    orf_tax_df.to_csv(sm.output.tax, sep="\t", index=True, header=True)
+
+
 def main(sm):
-    toolbox = {"tango_mash": tango_mash}
+    toolbox = {"tango_mash": tango_mash,
+               "tango_assign_orfs": tango_assign_orfs}
     toolbox[sm.rule](sm)
 
 
