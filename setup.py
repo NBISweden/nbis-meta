53e684247de58d74fc893af483ec87a112cb97ac
johnne <john.sundh@scilifelab.se>
2020-05-21 00:46:57 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+setuptools.setup(
+    name="nbis-meta",
+    version="0.0.1",
+    author="John Sundh",
+    author_email="john.sundh@scilifelab.se",
+    description="A snakemake workflow for metagenomics",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nbisweden/nbis-meta",
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages("src"),
+    package_data={"nbis-meta": ["workflow/*", "config/*"]},
+    entry_points={'console_scripts': ['nbis-meta = cli.__main__:main']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
