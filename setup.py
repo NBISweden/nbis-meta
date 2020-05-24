adb21d0dffe3d30117607940609b4e1bc8ff34f3
johnne <john.sundh@scilifelab.se>
2020-05-24 05:36:49 +0200

    Change imports

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_namespace_packages
 
 
 def readme():
@@ -6,25 +6,18 @@ def readme():
         return f.read()
 
 
-setuptools.setup(name="nbis-meta", version="0.0.1", author="John Sundh",
-                 author_email="john.sundh@scilifelab.se",
-                 description="A snakemake workflow for metagenomics",
-                 long_description=readme(),
-                 long_description_content_type="text/markdown",
-                 license="MIT",
-                 python_requires=">=3.7",
-                 install_requires=[
-                        "snakemake",
-                    ],
-                 url="https://github.com/nbisweden/nbis-meta",
-                 packages=["workflow"],
-                 package_data={"workflow": ["Snakefile", ".cfg/*", "envs/*",
-                                            "notebooks/*", "report/*",
-                                            "rules/*", "schemas/*",
-                                            "scripts/*"]},
-                 include_package_data=True,
-                 entry_points={
-                     'console_scripts': ['nmeta = workflow.__main__:main']},
-                 classifiers=["Programming Language :: Python :: 3",
-                              "License :: OSI Approved :: MIT License",
-                              "Operating System :: OS Independent", ], )
+setup(name="nbis-meta", version="0.0.4", author="John Sundh",
+      author_email="john.sundh@scilifelab.se",
+      description="A snakemake workflow for metagenomics",
+      long_description=readme(), long_description_content_type="text/markdown",
+      license="MIT", python_requires=">=3.7",
+      install_requires=["snakemake", ],
+      url="https://github.com/nbisweden/nbis-meta",
+      packages=["workflow"],
+      package_data={"workflow": ["Snakefile", ".cfg/*", "envs/*", "notebooks/*",
+                                 "report/*", "rules/*", "schemas/*",
+                                 "scripts/*"]},
+      entry_points={'console_scripts': ['nmeta = nmeta.__main__:main']},
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent", ], )
--- workflow/__main__.py
+++ workflow/__main__.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
-import snakemake
+from snakemake import snakemake
+from importlib import resources
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("-c", "--config", type=str,
-                        default="config/config.yaml",
-                        help="Config file")
+                        default="config/config.yaml", help="Configuration file")
     parser.add_argument("-n", "--dryrun", action="store_true")
     args, sm_args = parser.parse_known_args()
-    snakemake.snakemake("workflow/Snakefile", dryrun=args.dryrun,
-                        configfiles=[args.config])
+    with resources.path("workflow", "Snakefile") as snakefile:
+        s = snakemake(snakefile, dryrun=args.dryrun, configfiles=[args.config])
 
 
 if __name__ == "__main__":
