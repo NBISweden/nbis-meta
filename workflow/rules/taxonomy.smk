d1c636603975e05a40cd804a4a06d0a11fd0a8dd
johnne <john.sundh@scilifelab.se>
2023-02-08 21:47:21 +0100

    Update taxonomy rules
    Add mmseq download rule and rewrite assign_orfs to fit mmseq output

--- workflow/rules/taxonomy.smk
+++ workflow/rules/taxonomy.smk
@@ -1,28 +1,49 @@
 localrules:
-    taxonomy
+    taxonomy,
+    assign_orfs,
 
 
 ##### taxonomy master rule #####
 rule taxonomy:
     input:
         expand(
-            results + "/annotation/{assembly}/{db}.taxonomy.tsv",
+            results + "/annotation/{assembly}/{seqTaxDB}.taxonomy.tsv",
             assembly=assemblies.keys(),
-            db=config["taxonomy"]["database"],
+            seqTaxDB=config["taxonomy"]["database"],
         ),
+        expand(
+            results + "/annotation/{assembly}/{seqTaxDB}.orfs.taxonomy.tsv",
+            assembly=assemblies.keys(),
+            seqTaxDB=config["taxonomy"]["database"],
+        ),
+
 
 rule mmseq_downloadDB:
     output:
-        expand("resources/mmseqs/{{seqTaxDB}}{suff}",
-            suff=["", "_taxonomy", "_mapping", "_h.dbtype", "_h.index", "_h", ".lookup",
-                  ".dbtype", ".index"])
+        expand(
+            "resources/mmseqs/{{seqTaxDB}}{suff}",
+            suff=[
+                "",
+                "_taxonomy",
+                "_mapping",
+                "_h.dbtype",
+                "_h.index",
+                "_h",
+                ".lookup",
+                ".dbtype",
+                ".index",
+            ],
+        ),
     params:
-        seqTaxDB = "{seqTaxDB}",
+        seqTaxDB="{seqTaxDB}",
         tmpdir="$TMPDIR.{seqTaxDB}",
     threads: 4
     resources:
-        runtime = 24 * 60 * 3,
-        mem_mib = mem_allowed,
+        runtime=24 * 60 * 3,
+        mem_mib=mem_allowed,
+        slurm_account=lambda wildcards: config["slurm_account"]
+        if config["slurm_account"]
+        else None,
     envmodules:
         "bioinfo-tools",
         "MMseqs2/14-7e284",
@@ -34,6 +55,7 @@ rule mmseq_downloadDB:
         mmseqs databases  {params.seqTaxDB} {output[0]} {params.tmpdir} --threads {threads}
         rm -rf {params.tmpdir}
         """
+
 
 rule mmseqs_createqueryDB:
     output:
@@ -52,6 +74,12 @@ rule mmseqs_createqueryDB:
         "MMseqs2/14-7e284",
     conda:
         "../envs/mmseqs.yml"
+    resources:
+        runtime=60 * 2,
+        mem_mib=mem_allowed,
+        slurm_account=lambda wildcards: config["slurm_account"]
+        if config["slurm_account"]
+        else None,
     shell:
         """
         mmseqs createdb {input.fa} {output.queryDB[0]} --dbtype 2 --shuffle 0 \
@@ -61,7 +89,8 @@ rule mmseqs_createqueryDB:
 
 rule mmseqs_taxonomy:
     output:
-        expand(results + "/annotation/{{assembly}}/mmseqs/{{seqTaxDB}}{suff}",
+        expand(
+            results + "/annotation/{{assembly}}/mmseqs/{{seqTaxDB}}{suff}",
             suff=[".dbtype", ".index", "_aln.dbtype", "_aln.index"],
         ),
     input:
@@ -76,13 +105,19 @@ rule mmseqs_taxonomy:
         + wildcards.seqTaxDB,
         tmpdir="$TMPDIR/mmseqs.taxonomy.{assembly}",
         mem_mib=mem_allowed,
-        extra_params=config["taxonomy"]["mmseqs_extra_params"]
+        extra_params=config["taxonomy"]["mmseqs_extra_params"],
     threads: 20
     envmodules:
         "bioinfo-tools",
         "MMseqs2/14-7e284",
     conda:
         "../envs/mmseqs.yml"
+    resources:
+        runtime=60 * 10,
+        mem_mib=mem_allowed,
+        slurm_account=lambda wildcards: config["slurm_account"]
+        if config["slurm_account"]
+        else None,
     shell:
         """
         mkdir -p {params.tmpdir}
@@ -99,7 +134,7 @@ rule mmseqs_createtsv:
         queryDB=rules.mmseqs_createqueryDB.output.queryDB[0],
         taxRes=rules.mmseqs_taxonomy.output,
     log:
-        results + "/annotation/{assembly}/{seqTaxDB}.createtsv.log",
+        results + "/annotation/{assembly}/mmseqs/{seqTaxDB}.createtsv.log",
     params:
         taxRes=lambda wildcards, input: os.path.dirname(input.taxRes[0])
         + "/"
@@ -111,6 +146,12 @@ rule mmseqs_createtsv:
     conda:
         "../envs/mmseqs.yml"
     threads: 10
+    resources:
+        runtime=60 * 2,
+        mem_mib=mem_allowed,
+        slurm_account=lambda wildcards: config["slurm_account"]
+        if config["slurm_account"]
+        else None,
     shell:
         """
         mmseqs createtsv {input.queryDB} {params.taxRes} {output.lca} \
@@ -118,14 +159,14 @@ rule mmseqs_createtsv:
             --full-header 0 --idx-seq-src 0 --db-output 0 --compressed 0 -v 3  
         """
 
+
 rule assign_orfs:
     input:
-        tax=results + "/annotation/{assembly}/taxonomy/final_contigs.taxonomy.tsv",
+        tax=rules.mmseqs_createtsv.output.lca,
         gff=results + "/annotation/{assembly}/final_contigs.gff",
     output:
-        tax=expand(
-            results + "/annotation/{{assembly}}/taxonomy/orfs.{db}.taxonomy.tsv",
-            db=config["taxonomy"]["database"],
-        ),
+        tax=results + "/annotation/{assembly}/{seqTaxDB}.orfs.taxonomy.tsv",
+    params:
+        ranks=config["taxonomy"]["ranks"],
     script:
         "../scripts/taxonomy_utils.py"
--- workflow/scripts/taxonomy_utils.py
+++ workflow/scripts/taxonomy_utils.py
@@ -1,93 +1,45 @@
 #!/usr/bin/env python
 
-import sys
 import pandas as pd
 
 
-def add_lower(df, ranks):
+def add_lower(d, ranks):
     """
     Propagates assignments from higher to lower taxonomic ranks,
     and adds a 'Unclassified.' prefix.
-    :param df: pandas DataFrame
+    :param d: dictionary of taxonomic assignments
     :param ranks: ranks for which to propagate
     :return:
     """
-    for i in df.index:
-        last_known = df.loc[i, ranks[0]]
-        for rank in ranks[1:]:
-            if df.loc[i, rank] != "Unclassified":
-                last_known = df.loc[i, rank]
+    last_known = d[ranks[0]]
+    for rank in ranks[1:]:
+        if d[rank] != "unknown":
+            last_known = d[rank]
+        else:
+            if last_known == "Unclassified":
+                d[rank] = last_known
             else:
-                if last_known == "Unclassified":
-                    df.loc[i, rank] = last_known
-                else:
-                    df.loc[i, rank] = "Unclassified.{}".format(last_known)
-    return df
+                d[rank] = f"Unclassified.{last_known}"
+    return d
 
 
-def contigtax_mash(sm):
-    # Keep stats on assignments
-    # resolved = cases where sourmash helped resolve assignments
-    # transferred = cases where blast-based assignments were overwritten
-    # added = cases where assignments from sourmash were added
-    # total = total number of contigs
-    stats = {"resolved": 0, "transferred": 0, "added": 0, "total": 0}
-    df1 = pd.read_csv(sm.input.smash, sep=",", header=0, index_col=0)
-    stats["total"] = df1.shape[0]
-    df2 = pd.read_csv(sm.input.contigtax[0], sep="\t", header=0, index_col=0)
-    ranks = list(df2.columns)
-    ranks.reverse()
-    # Only use subset of contigs with matches
-    df1 = df1.loc[df1["status"] == "found", df2.columns]
-    df1.fillna("Unclassified", inplace=True)
-
-    # Get common set of contigs
-    common = set(df1.index).intersection(set(df2.index))
-    for contig in common:
-        s = df1.loc[contig]
-        b = df2.loc[contig]
-        for rank in ranks:
-            # If sourmash has an assignment at this rank
-            if s[rank] != "Unclassified":
-                # If blast-based contains 'Unclassified',
-                # mark contig as resolved
-                if "Unclassified" in b[rank]:
-                    stats["resolved"] += 1
-                # Otherwise, mark contig as transferred
-                else:
-                    stats["transferred"] += 1
-                # As soon as a contig has been transferred or resolved
-                # we can stop the merge
-                df2.loc[contig] = df1.loc[contig]
-                break
-            # If sourmash does not have an assignment at this rank
-            else:
-                # but blast-based does have an assignment,
-                # then the blast-based is more resolved and we can stop
-                # trying to merge
-                if "Unclassified" not in b[rank]:
-                    break
-    # Get contigs in sourmash missing from blast
-    missing1 = set(df1.index).difference(set(df2.index))
-    if len(missing1) > 0:
-        stats["added"] += len(missing1)
-        df2 = pd.concat([df2, df1.loc[missing1]])
-    df2 = add_lower(df2, df2.columns)
-    df2.to_csv(sm.output[0], sep="\t")
-    # Write to log
-    with open(sm.log[0], "w") as fhout:
-        fhout.write("Total:       {}\n".format(stats["total"]))
-        fhout.write("Resolved:    {}\n".format(stats["resolved"]))
-        fhout.write("Transferred: {}\n".format(stats["transferred"]))
-        fhout.write("Added:       {}\n".format(stats["added"]))
+def extract_lineage(d, ranks):
+    taxonomy = {}
+    for contig, lineage in d.items():
+        taxonomy[contig] = {}
+        for i, taxname in enumerate(lineage.split(";")):
+            taxonomy[contig][ranks[i]] = taxname
+        taxonomy[contig] = add_lower(taxonomy[contig], ranks)
+    return taxonomy
 
 
-def contigtax_assign_orfs(sm):
+def assign_orfs(sm):
     """
     Transfers taxonomic assignments from contigs down to ORFs called on contigs
     :param sm: snakemake object
     :return:
     """
+    ranks = sm.params.ranks
     gff_df = pd.read_csv(
         sm.input.gff,
         header=None,
@@ -105,10 +57,31 @@ def contigtax_assign_orfs(sm):
     ]
     gff_df.loc[:, "id"] = ids
     # Read taxonomy for contigs
-    tax_df = pd.read_csv(sm.input.tax, header=0, sep="\t", index_col=0)
+    tax_df = pd.read_csv(
+        sm.input.tax,
+        header=None,
+        index_col=0,
+        sep="\t",
+        names=[
+            "contig",
+            "taxid",
+            "rank",
+            "assignment",
+            "n_frags",
+            "label_frags",
+            "agree_frags",
+            "support",
+            "lineage",
+            "short_rank_lineage",
+        ],
+    )
+    # keep only contigs with assignments
+    tax_df = tax_df.loc[tax_df.lineage == tax_df.lineage]
+    taxdict = extract_lineage(tax_df.loc[:, "lineage"].to_dict(), ranks)
+    taxdf = pd.DataFrame(taxdict).T
     # Merge dataframes
     orf_tax_df = pd.merge(
-        gff_df, tax_df, left_on="contig", right_index=True, how="outer"
+        gff_df, taxdf, left_on="contig", right_index=True, how="outer"
     )
     # When using 'outer' merging there may be contigs with no called ORF
     # but with a tax assignment. Drop these contigs.
@@ -118,13 +91,12 @@ def contigtax_assign_orfs(sm):
     # Set index to ORF ids
     orf_tax_df.set_index("id", inplace=True)
     orf_tax_df.drop("contig", axis=1, inplace=True)
-    orf_tax_df.to_csv(sm.output.tax[0], sep="\t", index=True, header=True)
+    orf_tax_df.to_csv(sm.output.tax, sep="\t", index=True, header=True)
 
 
 def main(sm):
     toolbox = {
-        "merge_contigtax_sourmash": contigtax_mash,
-        "contigtax_assign_orfs": contigtax_assign_orfs,
+        "assign_orfs": assign_orfs,
     }
     toolbox[sm.rule](sm)
 
